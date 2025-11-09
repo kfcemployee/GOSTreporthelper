3d6ac8394faaddddb0bf3a819e0fb69a6cf55cfd
Your Name <sveducation14@gmail.com>
2025-11-09 19:08:02 +0300

    commit 2: updated file logic

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from parsers import DocParser, MdParser
 from converters import Converter, MarkdownConverter
 
-file_path = r""
+file_path = r"ny"
 
 if not file_path:
-    file_path = input("Введите путь к файлу: ")
+    try:
+        file_path = input("Введите путь к файлу: ")
+    except KeyboardInterrupt:
+        pass
 
 o_path = "__output.docx"
-
-if file_path.endswith(".txt"):
-    with open(file_path, encoding="utf-8") as text:
-        data = MdParser(text.read()).parse_()
-        d = MarkdownConverter(data, o_path).convert_to_doc()
-        p1 = DocParser(d)
-        Converter(d, p1.parse(), o_path).start()
-elif file_path.endswith(".docx"):
-    from docx import Document
-    try:
-        doc = Document(file_path)
-        p1 = DocParser(doc)
-        Converter(doc, p1.parse(), o_path).start()
-    except Exception as e:
-        print("Проверьте, точно ли ваш файл является docx. ", e)
-
-else:
-    print("Неподдерживаемый тип файла: может быть только docx или txt.")
+if file_path:
+    if file_path.endswith(".txt"):
+        with open(file_path, encoding="utf-8") as text:
+            data = MdParser(text.read()).parse_()
+            d = MarkdownConverter(data, o_path).convert_to_doc()
+            p1 = DocParser(d)
+            Converter(d, p1.parse(), o_path).start()
+    elif file_path.endswith(".docx"):
+        from docx import Document
+        try:
+            doc = Document(file_path)
+            p1 = DocParser(doc)
+            Converter(doc, p1.parse(), o_path).start()
+        except Exception as e:
+            print("Проверьте, точно ли ваш файл является docx. ", e)
+    else:
+        print("Файл не существует или тип файла не поддерживается.")
